06c8270519b150bdde2f7b099822e1d77cd40796
Lucas Mafra <lucas.mafra95@gmail.com>
2021-03-21 04:37:40 +0100

    add script to remove ads

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,6 +7,30 @@ import SEO from "../components/seo"
 import { Disqus, CommentCount } from 'gatsby-plugin-disqus'
 
 const BlogPostTemplate = ({ data, location }) => {
+  React.useEffect(() => {
+    console.log("using effect");
+    let disqus = document.getElementById('disqus_thread');
+
+    let remove_ads = setInterval(() => {
+        let iframes = document.getElementsByTagName('iframe');
+
+        for (var iframe in iframes) {
+            if (typeof iframes[iframe].src === 'undefined') {
+                continue;
+            }
+
+            if (iframes[iframe].src.match(/(ads-iframe)|(disqusads)/gi)) {
+                iframes[iframe].style.display = 'none';
+                disqus.style.width = '100%';
+            }
+        }
+    }, 500);
+
+    setTimeout(function () {
+        clearInterval(remove_ads);
+    }, 5000);
+  }, []);
+
   const post = data.markdownRemark
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const { previous, next } = data
