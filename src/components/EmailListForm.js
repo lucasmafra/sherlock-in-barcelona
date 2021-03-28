ed5393cc05d0474b9171dd20ebdf8ae65faa48e9
Lucas Mafra <lucas.mafra95@gmail.com>
2021-03-28 19:24:44 +0200

    style component

--- src/components/EmailListForm.js
+++ src/components/EmailListForm.js
@@ -2,30 +2,45 @@ import React, { useState } from 'react';
 import './EmailListForm.css';
 import addToMailchimp from 'gatsby-plugin-mailchimp';
 
+const SubscriptionResult = ({ result }) => {
+    const { msg, status } = result;
+    const statusColor = {
+        success: 'green',
+        error: 'red'
+    };
+    return <p style={{ marginTop: 16, fontWeight: 600, color: statusColor[status] }} dangerouslySetInnerHTML={{__html: msg}}/>;
+};
+
 const EmailListForm = () => {
 
   const [email, setEmail] = useState('');
+  const [subscriptionResult, setSubscriptionResult] = useState(null);
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-
     addToMailchimp(email)
       .then((data) => {
-        alert(data.result);
-      })
-          .catch((error) => {
-          console.log('error', error);
-      });
+        setEmail('');
+        setSubscriptionResult({
+            status: data.result,
+            msg: data.result === 'success' ? 'Inscrição feita com sucesso! Agora é só esperar pelo próximo post :)' : data.msg,
+        });
+      }).catch((error) => {
+          setSubscriptionResult({
+              status: 'error',
+              msg: error.msg
+        });
+     });
   };
 
   const handleEmailChange = (event) => {
+    setSubscriptionResult(null);
     setEmail(event.currentTarget.value);
   };
 
   return (
-    <form onSubmit={handleSubmit} className={"EmailListForm"}>
+    <div className={"EmailListForm"}>
       <h2>Inscreva-se em nossa newsletter!</h2>
-      <p>Nós atualizamos o blog semanalmente com posts sobre viagem e vida no exterior. <b>Nossos pais não perdem um post sequer.</b> Você também não vai querer perder!</p>
+      <p>Nós atualizamos o blog semanalmente com posts sobre viagem e a vida no exterior. <b>Nossos pais não perdem um post sequer.</b> Você também não vai querer perder!</p>
       <div className={'Wrapper'}>
         <input
           placeholder="Digite seu e-mail"
@@ -33,9 +48,10 @@ const EmailListForm = () => {
           type="text"
           onChange={handleEmailChange}
         />
-        <button type="submit">Inscrever-se</button>
+        <button onClick={handleSubmit}>Inscrever-se</button>
       </div>
-    </form>
+      {subscriptionResult && <SubscriptionResult result={subscriptionResult}/>}
+    </div>
   );
 };
 
